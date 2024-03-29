549509bcd43f98d9f0e485205f1a18a6fc8eead1
killpathhh <144938343+legitplaya@users.noreply.github.com>
2024-03-30 00:20:17 +0300

    c#

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.Write("Введите первое значение: ");
+        int startValue = int.Parse(Console.ReadLine());
+        Console.Write("Введите первое значение: ");
+        int endValue = int.Parse(Console.ReadLine());
+
+        bool[] prime = new bool[endValue+1];
+
+        for (int i = 0; i<=endValue; i++)
+            prime[i] = true;
+
+        for (int i = 2; i * 2 <=endValue; i++)
+        {
+
+            if (prime[i])
+            {
+                for(int p = 2 * i; p<=endValue; p+=i)
+                {
+                    prime[p] = false;
+                }
+                
+            }
+
+        }
+        int sum = 0;
+
+        for (int i = Math.Max(2,startValue); i <= endValue; i++)
+        {
+            if (prime[i])
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"Сумма простых чисел = {sum}");
+    }
+}
